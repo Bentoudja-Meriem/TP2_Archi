f67973be7f25a459b8f0f03dbc7ae70c1bebabb1
Bentoudja-Meriem <meriomamerioma2020@gmail.com>
2022-12-04 17:53:24 +0100

    question 2

---
+++ DBConnection.java
@@ -0,0 +1,42 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+
+	    private static DBConnection instance ;
+		
+	   
+		private BDD = "nomBD";
+		private url = "jdbc:mysql://localhost:3306/" + BDD;
+		private user = "root";
+		private passwd = "";
+	    public Connection conn;
+
+        private DBConnection(){}
+
+		public static DBConnection getInstance(){
+			if (instance == null){
+				instance= new DBConnection();
+			}
+		}
+
+		public Connection getConn(){
+			return conn=DriverManager.getConnection();
+		}
+		
+	       
+
+		}
+
+	    
+	   /* public Connection getConn() {
+			return conn;
+		}*/
+
+ /*public DBConnection() throws SQLException {
+			conn=DriverManager.getConnection(url, user,passwd);*/
+		
+	
+}
